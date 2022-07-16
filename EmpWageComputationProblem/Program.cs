66876576090e5eab7c47460e70f1c420bd095652
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-16 13:55:21 +0530

    UC-1 Employee attendance

--- EmpWageComputationProblem/Program.cs
+++ EmpWageComputationProblem/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Welcome to Employee Wage Computation Program");
+﻿int IS_FULL_TIME = 1;
+Random random = new Random();
+int empCheck = random.Next(0, 2);
+if (empCheck == IS_FULL_TIME)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
