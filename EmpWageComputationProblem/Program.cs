d4f692964eb4c44a5cc410bd0b0e58898fbba3cd
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-20 20:22:44 +0530

    Comments added in CheckAttendance Program

--- EmpWageComputationProblem/Program.cs
+++ EmpWageComputationProblem/Program.cs
@@ -1,11 +1,11 @@
-﻿int IS_FULL_TIME = 1;
-Random random = new Random();
-int empCheck = random.Next(0, 2);
-if (empCheck == IS_FULL_TIME)
+﻿int IS_FULL_TIME = 1; // the value 1 is assign to variable IS_FULL_TIME 
+Random random = new Random(); //creating a Random class object to call the methods inside this class
+int empCheck = random.Next(0, 2); //using random.Next method we are generating random values 0 or 1
+if (empCheck == IS_FULL_TIME) // In if condition, we are checking whether empCheck value is equals to IS_FULL_TIME value
 {
-    Console.WriteLine("Employee is Present");
+    Console.WriteLine("Employee is Present"); //if condition becomes true then "Employee is present" is display
 }
-else
+else // if the condition becomes false then it comes to else part and displays "Employee is absent"
 {
     Console.WriteLine("Employee is Absent");
 }
